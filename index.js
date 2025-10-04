c13ef320437d0b31165b2358d4a4a8adceba394a
Shpak Markiian bc2025-3 <shpakmark@gmail.com>
2025-10-04 18:28:14 +0300

    Added functionality according to second task.

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const { program } = require('commander');
+const fs = require('fs');
 
 program
-	.requiredOption('-i, --input <path>')
-	.option('-o, --output <path>')
-	.option('-d, --display');
+	.requiredOption('-i, --input <path>', 'path to input file')
+	.option('-o, --output <path>', 'path to output file')
+	.option('-d, --display', 'print result in console')
+	.option('-v, --variety', 'display flower variety')
+	.option(
+		'-l, --length <number>',
+		'filter: petal length more than given',
+		parseFloat
+	);
 
 program.parse();
 const options = program.opts();
+
+if (!options.input) {
+	console.error('Please, specify input file');
+	process.exit(1);
+};
+
+if (!fs.existsSync(options.input)) {
+	console.error('Cannot find input file');
+	process.exit(1);
+};
+
+const rawData = fs.readFileSync(options.input, 'utf8');
+let data = rawData
+	.split('\n')
+	.filter(line => line.trim().length > 0)
+	.map(line => JSON.parse(line));
+
+if (options.length)
+	data = data.filter(item => item['petal.length'] > options.length);
+
+let result = '';
+data.forEach(item => {
+	let line = `${item['petal.length']} ${item['petal.width']}`;
+	if (options.variety)
+		line += ` ${item.variety}`;
+	result += line + '\n';
+});
+
+if (options.display)
+	console.log(result);
+
+if (options.output)
+	fs.writeFileSync(options.output, result);
 
